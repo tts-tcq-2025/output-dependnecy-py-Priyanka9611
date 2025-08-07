72ebc4d1959b54fb749354b1bfe2ed3b34a3e18b
Priyanka9611 <Priyanka.Kalakutagi@in.bosch.com>
2025-08-07 09:46:29 +0530

    Update weatherreport.py

--- weatherreport.py
+++ weatherreport.py
@@ -21,6 +21,65 @@ def report(sensorReader):
     return weather
 
 
+def testPartlyCloudy():
+    def pcStub():
+        return {
+            'temperatureInC': 27,
+            'precipitation': 20,
+            'humidity': 20,
+            'windSpeedKMPH': 15
+        }
+    weather = report(pcStub)
+    print(weather)
+    assert weather == "Partly Cloudy"
+    
+    def pcStub():
+        return {
+            'temperatureInC': 30,
+            'precipitation': 59,
+            'humidity': 20,
+            'windSpeedKMPH': 15
+        }
+    weather = report(pcStub)
+    print(weather)
+    assert weather == "Partly Cloudy"
+
+
+def testSunnyDay():
+    def sunnyStub():
+        return {
+            'temperatureInC': 25,
+            'precipitation': 30,
+            'humidity': 20,
+            'windSpeedKMPH': 60
+        }
+    weather = report(sunnyStub)
+    print(weather)
+    assert weather == "Sunny Day"
+    
+    def sunnyStub():
+        return {
+            'temperatureInC': 15,
+            'precipitation': 10,
+            'humidity': 20,
+            'windSpeedKMPH': 15
+        }
+    weather = report(sunnyStub)
+    print(weather)
+    assert weather == "Sunny Day"
+    
+    def sunnyStub():
+        return {
+            'temperatureInC': 60,
+            'precipitation': 10,
+            'humidity': 20,
+            'windSpeedKMPH': 15
+        }
+    weather = report(sunnyStub)
+    print(weather)
+    assert weather == "Sunny Day"
+
+
 def testRainy():
     weather = report(sensorStub)
     print(weather)
@@ -36,9 +95,21 @@ def testHighPrecipitation():
     # strengthen the assert to expose the bug
     # (function returns Sunny day, it should predict rain)
     assert(len(weather) > 0);
+    
+    def humidityStub():
+        return {
+            'temperatureInC': 50,
+            'precipitation': 70,
+            'humidity': 26,
+            'windSpeedKMPH': 10
+            }
+    weather = report(humidityStub)
+    assert('rain' in weather);
 
 
 if __name__ == '__main__':
     testRainy()
     testHighPrecipitation()
+    testSunnyDay()
+    testPartlyCloudy()
     print("All is well (maybe!)");
