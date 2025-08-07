b1d6005d8758d98124e5c6748d318a4997202322
Priyanka9611 <Priyanka.Kalakutagi@in.bosch.com>
2025-08-07 09:43:47 +0530

    alerter.py

--- alerter.py
+++ alerter.py
@@ -6,7 +6,29 @@ def network_alert_stub(celcius):
     # Return 500 for not-ok
     # stub always succeeds and returns 200
     return 200
+    
+def test_failed_alert(f):
+    global alert_failure_count, network_alert_stub
+    alert_failure_count = 0
 
+    def fail_stub(c):
+        return 500
+
+    network_alert_stub = fail_stub
+    alert_in_celcius(f)
+    assert alert_failure_count == 1
+    
+def test_success_alert(f):
+    global alert_failure_count, network_alert_stub
+    alert_failure_count = 0
+
+    def success_stub(c):
+        return 500
+
+    network_alert_stub = success_stub
+    alert_in_celcius(f)
+    assert alert_failure_count == 0
+    
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
     returnCode = network_alert_stub(celcius)
@@ -21,5 +43,7 @@ def alert_in_celcius(farenheit):
 
 alert_in_celcius(400.5)
 alert_in_celcius(303.6)
+test_failed_alert(400.5)
+test_success_alert(300)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
